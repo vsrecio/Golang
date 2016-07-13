d9527a98bba6d57face934a4b6658d2a25c5f9f6
vsrecio <starlyn.recio@gmail.com>
2016-07-13 10:46:32 -0400

    Arrays

--- arrays.go
+++ arrays.go
@@ -3,12 +3,28 @@ package main
 import "fmt"
 
 func main()  {
-  // var x [25]int
-  // x[10] = 100
-  // fmt.Println(x)
+  // Array of int with a length of 10
+  arreglo := [10]int{1,2,3,4}
+  fmt.Println("the length of array is", len(arreglo)) // we can print the length with len
 
-// Leccion de los slide
-  slide1 := []int{1,2,3}
-  slide2 := append(slide1, 4, 5)
-  fmt.Println(slide2)
+  // Furthermore, we can make Matrix with our array
+  var matriz [5][2]int // Matrix of 5 array with e elements
+  matriz[0][1] = 2
+  fmt.Println("the matrix is", matriz)
+  fmt.Println("We are inide of", matriz[0][1])
+
+  // This is a program that computes the average of a series os scores.
+  var x [5]float64
+      x[0] = 98
+      x[1] = 99
+      x[2] = 89
+      x[3] = 90
+      x[4] = 84
+
+  var total float64 = 0
+  for i := 0; i < 5; i++ {
+    total += x[i]
+  }
+  // We convert the type of x that is int to float64
+  fmt.Println("the average is", total / float64(len(x)))
 }
